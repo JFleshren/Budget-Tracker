944f1aa9eb0a842a653bb25f359e450f77d15ce5
Fleshren <fleshren1@gmail.com>
2024-01-10 21:32:41 -0600

    Basic SWL and Node added

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS budget_db;
+CREATE DATABASE budget_db;
+-- Makes it so all of the following code will affect budget_db --
+USE budget_db;
+
+CREATE TABLE categories (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE transactions (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  category_id INT,
+  description VARCHAR(255) NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  date DATE NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
