8f76468d6533dbef19ae5f704a4de882c48681a4
elroy <devinjl665@gmail.com>
2024-01-22 14:39:15 -0600

    fixing some small errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const userRoutes = require('./userRoutes')
-const TransactionsRoutes = require('./transactionsRoutes')
+const transactionsRoutes = require('/transactionsRoutes')
 
 router.use('/users', userRoutes)
-router.use('/transactions', TransactionsRoutes)
+router.use('/transactions', transactionsRoutes)
 
 module.exports = router
