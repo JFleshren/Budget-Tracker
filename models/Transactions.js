d835a23dddc00e65923316bfcc9b9645bf063c4c
Fleshren <fleshren1@gmail.com>
2024-01-21 21:23:22 -0600

    fixes to transactions.js

--- models/Transactions.js
+++ models/Transactions.js
@@ -1,43 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const Category = require('./Category');
 
 class Transactions extends Model {}
+
 Transactions.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      amount: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'transactions',
-    }
-  );
-  // Connect to category model
-  Transactions.belongsTo(Category, {
-    foreignKey: 'category_id',
-  });
-  
-  module.exports = Transactions;
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'transactions',
+  }
+);
+
+// Connect to category model
+Transactions.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+module.exports = Transactions;
