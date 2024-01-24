1340d4a995892ae66cb6ad6fb67b24416ac338f1
Fleshren <fleshren1@gmail.com>
2024-01-23 20:23:40 -0600

    reverted one path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const userRoutes = require('./userRoutes')
 
 
-const transactionsRoutes = require('/transactionsRoutes')
+const transactions = require('./transactions.js');
 
 router.use('/users', userRoutes)
 router.use('./transactions', transactionsRoutes)
