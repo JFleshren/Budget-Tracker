f85fd1e0d6ea6d6c3b7c0ca5fb3f90c34357922f
Fleshren <fleshren1@gmail.com>
2024-01-23 20:25:01 -0600

    posible fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const userRoutes = require('./userRoutes')
 
 
-const transactions = require('./transactions.js'); 
+const transactions = require('./transactions.js');
 
 router.use('/users', userRoutes)
 router.use('./transactions', transactionsRoutes)
