115935cf8ac1a85cfccb1e28451e1eb9213fea5d
elroy <devinjl665@gmail.com>
2024-01-23 20:04:22 -0600

    changed back casing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const transactionsRoutes = require('./transactionsRoutes'); // Corrected import path
+const transactionsRoutes = require('./TransactionsRoutes'); // Corrected import path
 
 router.use('/users', userRoutes);
 router.use('/transactions', transactionsRoutes);
